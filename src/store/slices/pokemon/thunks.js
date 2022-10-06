9aa1bef773d9025681a22e5edab8dbac2ed3ae53
David <david.villegas@f-alternative.com>
2022-10-06 19:12:36 +0200

    prueba commit

---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,17 @@
+import { pokemonApi } from "../../../api/pokemonApi";
+import { setPokemons, startLoagingPokemon } from "./pokemonSlice"
+
+
+export const getPokemons = (page = 0) => {
+    return async (dispatch, getState) => {
+        dispatch(startLoagingPokemon());
+
+
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+
+        //   const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`)
+        //   const data = await resp.json();
+        //console.log(data);  
+        dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
+    }
+}
